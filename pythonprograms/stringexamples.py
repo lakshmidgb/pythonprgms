792c2fe124233666dcfadb55b02c94738f8bf15a
lakshmidgb <36836824+lakshmidgb@users.noreply.github.com>
2018-06-23 18:33:45 +0530

    Add files via upload

--- pythonprograms/stringexamples.py
+++ pythonprograms/stringexamples.py
@@ -4,7 +4,12 @@ Created on Fri Jun 22 19:00:56 2018
 
 @author: lakshmi
 """
+#Replaces all v and b in the original sentence with b and v respectively
 s=input("Enter a sentence :")
-snew = s.replace('v', 'b')
-print(snew) 
-print(s.replace('b', 'v'))
+for ch in s:
+    if ch =='v':
+        s = s.replace('v', 'v1')
+    s1 = s.replace('b', 'v')
+    s2 = s1.replace('v1','b')
+print(s2) 
+#print(s.replace('b', 'v'))
